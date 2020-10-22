b49f499406a04fa5273c4fe0d2000268b87ba69b
李譯騏 <patrick14754@gmail.com>
2020-10-23 01:20:47 +0800

    練習1:設定Route反應

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/',function(){
+    return'welcome';
+});
+Route::get('/',function(){
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+    return redirect('welcome');
+});
