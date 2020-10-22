1122c506075fb49675313874e0f1b34f7a39c397
李譯騏 <patrick14754@gmail.com>
2020-10-23 01:32:23 +0800

    練習5:設定Route前置

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,13 @@ Route::get('hello/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
 Route::get('say/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
     return view('welcome');
 }]);
+
+Route::get('dashboard',function(){
+    return'dashboard';
+}]);
+
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'dashboard';
+    });
+});
