f671179787c02b18038b23e85532cc4557c2d28b
李譯騏 <patrick14754@gmail.com>
2020-10-23 01:30:58 +0800

    練習4:Route命名

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::get('/',function(){
     return view('welcome');
 });
 Route::get('/', function () {
-    return view('welcome');
     return redirect('welcome');
 });
 
@@ -31,4 +30,10 @@ Route::get('hello/{name?}',function($name='Everybody'){
     return'Hello,'.$name;
 });
 
+Route::get('hello/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
+    return'Hello,'.$name;
+}]);
 
+Route::get('say/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
+    return view('welcome');
+}]);
