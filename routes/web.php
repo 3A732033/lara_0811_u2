65bb4046c1e3028f89e2e35129c121a17964b84b
李譯騏 <patrick14754@gmail.com>
2021-01-11 01:11:04 +0800

    練習 1：設定 Route 反應 修正

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/',function(){
-    return'welcome';
+    return redirect('r1');
 });
-Route::get('/',function(){
+
+Route::get('r1', function(){
+    return redirect('r2');
+});
+Route::get('r2', function(){
     return view('welcome');
-});
-Route::get('/', function () {
-    return redirect('welcome');
 });
 
 Route::get('hello/{name?}',function($name='Everybody'){
@@ -48,8 +46,8 @@ Route::group(['prefix'=>'admin'], function () {
     });
 });
 
-Route::get('/', function () {
-    return redirect('welcome');
-});
-use App\Http\Controllers\HomeController;
+//Route::get('/', function () {
+//    return redirect('welcome');
+//});
+//use App\Http\Controllers\HomeController;
 Route::get('user/{id}',[HomeController::class,'show']);
